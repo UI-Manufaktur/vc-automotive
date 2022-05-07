5a6a005396302e8c66018cef8163d150bc0d7b5c
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-05-07 06:57:42 +0200

    Initial works

--- source/vcs/automotive/controllers/package.d
+++ source/vcs/automotive/controllers/package.d
@@ -1,1 +1,4 @@
 module vcs.automotive.controllers;
+
+public import vcs.automotive.controllers.actions;
+public import vcs.automotive.controllers.pages;
---
+++ source/vcs/automotive/controllers/pages/package.d
@@ -0,0 +1,1 @@
+module vcs.automotive.controllers.pages;
